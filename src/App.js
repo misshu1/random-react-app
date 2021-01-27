b2389a8a9f6333eb2dd7273d947405236228bbd2
misshu1 <nedelusandrei@gmail.com>
2021-01-27 18:34:59 +0200

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { GlobalStyles } from 'components/style';
 import { RoutesApp } from 'components/routes/Routes';
 
 export function App() {
     return (
         <>
+            <GlobalStyles />
             <RoutesApp />
         </>
     );
---
+++ src/components/style/GlobalStyles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyles = createGlobalStyle`
+${normalize}
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    height: 100%;
+    font-size: 16px;
+}
+
+body {
+    font-family: 'Roboto', sans-serif;
+    position: relative;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+
+}
+
+`;
